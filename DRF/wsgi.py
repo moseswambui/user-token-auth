9ada88aa2065639b3bc5815aea0da4eff98f5898
Moses <moseswambui044@gmail.com>
2021-10-22 11:02:19 +0300

    wsgi 2

--- DRF/wsgi.py
+++ DRF/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DRF.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DRF/DRF.settings')
 
 application = get_wsgi_application()
